deadf3d8c3f5fc1cf610616549b6cfee6c2e6372
Vahab <108904622+vahabmehdiyev@users.noreply.github.com>
2023-07-18 03:54:15 +0400

    EmailSender

--- emailsender.py
+++ emailsender.py
@@ -2,21 +2,28 @@ from email.message import EmailMessage
 import ssl
 import smtplib
 
+
+
 email_sender = ''
 email_password = ''
 email_receiver = ''
 
 subject = 'Python Developer'
-body = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
+body = """
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+
+
+"""
 
 em = EmailMessage()
 em['From'] = email_sender
 em['To'] = email_receiver
-em['Subject'] = subject
+em['subject'] = subject
 em.set_content(body)
 
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
-    smtp.send_message(em)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
